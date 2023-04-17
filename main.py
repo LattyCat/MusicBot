af2944b9cc3aee5aac5c69ae24bdead0671de3af
Atsushi Tashiro <selenite.white@gmail.com>
2023-04-17 22:04:57 +0000

    fix bug

--- main.py
+++ main.py
@@ -30,24 +30,39 @@ async def on_message(message):
                 "You need to connect to a voice channel first.")
             return
         else:
-            # ボイスチャンネルに参加
-            print('voice channelに接続します')
-            voice_client = await voice_channel.connect()
-            print('voice channelに接続しました')
+            # Check if the bot is already connected to a voice channel
+            voice_client = discord.utils.get(client.voice_clients, guild=message.guild)
+            if voice_client and voice_client.is_playing():
+                # Stop the current audio playback
+                voice_client.stop()
+
+            if not voice_client:
+                # ボイスチャンネルに参加
+                try:
+                    print('voice channelに接続します')
+                    voice_client = await voice_channel.connect()
+                    print('voice channelに接続しました')
+                except Exception as e:
+                    print(f'Error connecting to voice channel: {e}')
+                    return
 
             # Youtubeから音声をダウンロードして再生
-            ydl_opts = {
-                'format': 'bestaudio/best',
-                'postprocessors': [{
-                    'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'mp3',
-                    'preferredquality': '128',
-                }]
-            }
-            with YoutubeDL(ydl_opts) as ydl:
-                info_dict = ydl.extract_info(url, download=False)
-                url2 = info_dict.get("url", None)
-                source = await discord.FFmpegOpusAudio.from_probe(url2)
-                voice_client.play(source)
+            try:
+                ydl_opts = {
+                    'format': 'bestaudio/best',
+                    'postprocessors': [{
+                        'key': 'FFmpegExtractAudio',
+                        'preferredcodec': 'mp3',
+                        'preferredquality': '128',
+                    }]
+                }
+                with YoutubeDL(ydl_opts) as ydl:
+                    info_dict = ydl.extract_info(url, download=False)
+                    url2 = info_dict.get("url", None)
+                    source = await discord.FFmpegOpusAudio.from_probe(url2)
+                    voice_client.play(source)
+            except Exception as e:
+                print(f'Error playing audio: {e}')
+                return
 
 client.run(config['Credentials']['Token'])
