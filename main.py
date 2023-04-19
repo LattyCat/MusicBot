dcd7f8d232c2b221d119b012dbc11e512489c339
lattechi <130142484+lattechi@users.noreply.github.com>
2023-04-19 20:33:51 +0900

    Develop (#8)
    * Add !stop command & リファクタリング (#4)
    
    * add discord bot command !play (#1)
    
    * fix flake8
    
    * [fix]bug
    
    * fix bug
    
    * move run.py
    
    * delete options.yaml in .gitignore
    
    * change message
    
    * add `!stop` command
    
    * リファクタリング
    
    * YouTube音声が終了した時に音声チャネルにユーザがいなかった場合、Botを切断するコードを追加 (#5)
    
    * change lower token
    
    * delete pip3 install --upgrade pip in Dockerfile
    
    * add volume command
    
    * add 誰もいない場合に5分後BotをDisconnectする機能を追加
    
    * リファクタリング
    
    * fix bug 5分後になってもBotが切断されない
    
    * fix bug
    
    * fix flake8 (#7)

--- main.py
+++ main.py
@@ -1,81 +1,119 @@
+import asyncio
 import configparser
-
 import discord
+from discord.ext import commands
 from yt_dlp import YoutubeDL
-
 
 config = configparser.ConfigParser()
 config.read('config/options.ini')
 
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+volume_transformer = None
 
 
 async def join_voice_channel(voice_channel):
-    try:
-        print('voice channelに接続します')
-        voice_client = await voice_channel.connect()
-        print('voice channelに接続しました')
-        return voice_client
-    except Exception as e:
-        print(f'Error connecting to voice channel: {e}')
-        return None
+    print('voice channelに接続します')
+    voice_client = await voice_channel.connect()
+    print('voice channelに接続しました')
+    return voice_client
 
 
-async def play_audio(
-        voice_client, url,
-        default_volume=int(config['Player']['DefaultVolume'])
-        ):
+async def play_audio(voice_client, url, default_volume=int(
+        config['player']['default_volume'])):
+    global volume_transformer
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'opus',
+            'preferredquality': '192',
+        }]
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        url2 = info_dict.get("url", None)
+        source = discord.FFmpegPCMAudio(url2, options="-acodec pcm_s16le")
+        volume_transformer = discord.PCMVolumeTransformer(
+            source, volume=default_volume / 100)
+        voice_client.play(volume_transformer, after=lambda e:
+                          asyncio.run_coroutine_threadsafe(
+                            on_play_finished(voice_client), bot.loop))
+    asyncio.create_task(disconnect_if_empty(voice_client))
+
+
+async def on_play_finished(voice_client):
+    if not voice_client.is_playing():
+        asyncio.create_task(disconnect_if_empty(voice_client))
+
+
+async def disconnect_if_empty(voice_client):
+    await asyncio.sleep(30)  # 30 seconds
+    if not voice_client.is_playing() \
+            and len(voice_client.channel.members) == 1:
+        await voice_client.disconnect()
+
+
+async def user_in_voice_channel(ctx):
+    if ctx.author.voice is None or ctx.author.voice.channel is None:
+        await ctx.send("音声チャンネルに接続してからコマンドを実行してください")
+        return False
+    return True
+
+
+def change_volume(new_volume):
+    global volume_transformer
+    if volume_transformer is not None:
+        volume_transformer.volume = new_volume / 100
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+@bot.command()
+async def play(ctx, url):
+    if not await user_in_voice_channel(ctx):
+        return
+
+    voice_channel = ctx.author.voice.channel
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice_client and voice_client.is_playing():
+        voice_client.stop()
+
+    if not voice_client:
+        try:
+            voice_client = await join_voice_channel(voice_channel)
+        except Exception as e:
+            print(f'Error connecting to voice channel: {e}')
+            return
+
     try:
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '128',
-            }]
-        }
-        with YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(url, download=False)
-            url2 = info_dict.get("url", None)
-            source = await discord.FFmpegOpusAudio.from_probe(url2)
-            volume_transformer = discord.PCMVolumeTransformer(
-                source, volume=default_volume/100)
-            voice_client.play(volume_transformer)
+        default_volume = int(config['player']['default_volume'])
+        await play_audio(voice_client, url, default_volume)
     except Exception as e:
         print(f'Error playing audio: {e}')
 
 
-@client.event
-async def on_ready():
-    print('Logged in as {0.user}'.format(client))
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+@bot.command()
+async def stop(ctx):
+    if not await user_in_voice_channel(ctx):
         return
 
-    if message.content.startswith('!play'):
-        url = message.content.split()[1]
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice_client and voice_client.is_playing():
+        voice_client.stop()
 
-        if message.author.voice is None or \
-                message.author.voice.channel is None:
-            await message.channel.send(
-                "You need to connect to a voice channel first.")
-            return
 
-        voice_channel = message.author.voice.channel
-        voice_client = discord.utils.get(
-            client.voice_clients, guild=message.guild)
-        if voice_client and voice_client.is_playing():
-            voice_client.stop()
+@bot.command()
+async def volume(ctx, new_volume: int):
+    if new_volume < 0 or new_volume > 100:
+        await ctx.send("ボリュームは0から100の範囲で指定してください。")
+        return
 
-        if not voice_client:
-            voice_client = await join_voice_channel(voice_channel)
-            if not voice_client:
-                return
+    change_volume(new_volume)
+    await ctx.send(f"ボリュームを{new_volume}%に設定しました。")
 
-        await play_audio(voice_client, url)
-
-client.run(config['Credentials']['Token'])
+bot.run(config['credentials']['token'])
