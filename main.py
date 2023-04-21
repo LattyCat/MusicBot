ccb48279f71fced26d5f7b47bd120907b263bd87
Atsushi Tashiro <selenite.white@gmail.com>
2023-04-21 03:15:53 +0000

    Global variables are capitalized.

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import discord
 from discord.ext import commands
 from yt_dlp import YoutubeDL
 
-config = configparser.ConfigParser()
-config.read('config/options.ini')
+CONFIG = configparser.ConfigParser()
+CONFIG.read('config/options.ini')
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
-volume_transformer = None
+INTENTS = discord.Intents.default()
+INTENTS.message_content = True
+BOT = commands.Bot(command_prefix='!', intents=INTENTS)
+VOLUME_TRANSFORMER = None
 
 
 # Voice channelへの接続
@@ -23,8 +23,8 @@ async def join_voice_channel(voice_channel):
 
 # Audioの再生
 async def play_audio(voice_client, url, default_volume=int(
-        config['player']['default_volume'])):
-    global volume_transformer
+        CONFIG['player']['default_volume'])):
+    global VOLUME_TRANSFORMER
     ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
@@ -41,11 +41,11 @@ async def play_audio(voice_client, url, default_volume=int(
 
     # Play the audio
     source = discord.FFmpegPCMAudio(url2, options="-acodec pcm_s16le")
-    volume_transformer = discord.PCMVolumeTransformer(
+    VOLUME_TRANSFORMER = discord.PCMVolumeTransformer(
         source, volume=default_volume / 100)
-    voice_client.play(volume_transformer, after=lambda e:
+    voice_client.play(VOLUME_TRANSFORMER, after=lambda e:
                       asyncio.run_coroutine_threadsafe(
-                        disconnect_after_timeout(voice_client), bot.loop))
+                        disconnect_after_timeout(voice_client), BOT.loop))
     asyncio.create_task(disconnect_after_timeout(voice_client))
 
 
@@ -66,25 +66,25 @@ async def disconnect_after_timeout(voice_client, timeout=180):
 
 # 音量変更
 def change_volume(new_volume):
-    global volume_transformer
-    if volume_transformer is not None:
-        volume_transformer.volume = new_volume / 100
+    global VOLUME_TRANSFORMER
+    if VOLUME_TRANSFORMER is not None:
+        VOLUME_TRANSFORMER.volume = new_volume / 100
 
 
 # pingコマンド
-@bot.command()
+@BOT.command()
 async def ping(ctx):
     await ctx.send('pong')
 
 
 # 再生コマンド
-@bot.command()
+@BOT.command()
 async def play(ctx, url):
     if not await user_in_voice_channel(ctx):
         return
 
     voice_channel = ctx.author.voice.channel
-    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice_client = discord.utils.get(BOT.voice_clients, guild=ctx.guild)
     if voice_client and voice_client.is_playing():
         voice_client.stop()
 
@@ -96,26 +96,26 @@ async def play(ctx, url):
             return
 
     try:
-        default_volume = int(config['player']['default_volume'])
+        default_volume = int(CONFIG['player']['default_volume'])
         await play_audio(voice_client, url, default_volume)
     except Exception as e:
         print(f'Error playing audio: {e}')
 
 
 # 停止コマンド
-@bot.command()
+@BOT.command()
 async def stop(ctx):
     if not await user_in_voice_channel(ctx):
         return
 
-    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice_client = discord.utils.get(BOT.voice_clients, guild=ctx.guild)
     if voice_client and voice_client.is_playing():
         voice_client.stop()
         asyncio.create_task(disconnect_after_timeout(voice_client))
 
 
 # ボリューム変更コマンド
-@bot.command()
+@BOT.command()
 async def volume(ctx, new_volume: int):
     if new_volume < 0 or new_volume > 100:
         await ctx.send("ボリュームは0から100の範囲で指定してください。")
@@ -124,4 +124,4 @@ async def volume(ctx, new_volume: int):
     change_volume(new_volume)
     await ctx.send(f"ボリュームを{new_volume}%に設定しました。")
 
-bot.run(config['credentials']['token'])
+BOT.run(CONFIG['credentials']['token'])
